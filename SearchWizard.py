aff4f1a4d7d122b86361c5ce69a5ec11efe34529
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-08-10 23:25:29 +0300

    Update SearchWizard.py

--- SearchWizard.py
+++ SearchWizard.py
@@ -3,10 +3,20 @@
 #              to key elements provided by the user                                                        #
 #              The functions in this class implement the 3rd requirement of the assignment        #
 ############################################################################################################
-import ast
-
+import streamlit as st
 from DB_handler import DB_handler
+import pandas as pd
+from datetime import *
 
+
+def parse_date(date_str_inp):
+    try:
+        # Try to parse the date string using the specified format
+        ret = datetime.strptime(date_str_inp, '%B %d, %Y')
+        return ret
+    except ValueError:
+        # If a ValueError is raised, the date string does not match the format
+        return None
 
 def parse_tuples_string(string_representation):
     # Remove outer curly braces and split the string into individual tuple strings
@@ -110,10 +120,55 @@ class SearchWizard:
 
 
 
-
     ## End of the cluster of functions for requirement number 3
 
     ## The following cluster of functions is supposed to take care of requirement number 4
     #  in the project's requirements. The cluster ends in the next ##
     # def
     ## End of the cluster of functions for requirement number 4
+    def handle_search_reporter_articles(self):
+        reporter_name = st.text_input("Please enter a reporter's name: ")
+        articles_of_reporter = self.search_reporter_articles(reporter_name)
+        if articles_of_reporter is not None and len(articles_of_reporter) != 0:
+            df = pd.DataFrame(articles_of_reporter, columns=["Article Title", "Newspaper", "Date"])
+            st.subheader(f"Articles written by {reporter_name}: ")
+            st.dataframe(df, hide_index=True)
+        elif articles_of_reporter is not None and len(articles_of_reporter) == 0:
+            st.error("No articles found.")
+
+    def handle_search_newspaper_articles(self):
+        newspaper_name = st.text_input("Please enter a newspaper's name: ")
+        articles_of_newspaper = self.search_np_articles(newspaper_name)
+        if articles_of_newspaper is not None and len(articles_of_newspaper) != 0:
+            df = pd.DataFrame(articles_of_newspaper, columns=["Article Title", "Date"])
+            st.subheader(f"Articles in {newspaper_name}: ")
+            st.dataframe(df, hide_index=True)
+        elif articles_of_newspaper is not None and len(articles_of_newspaper) == 0:
+            st.error("No articles found.")
+        elif articles_of_newspaper is None and len(newspaper_name) != 0:
+            st.error("Invalid newspaper")
+
+    def handle_search_date_articles(self):
+        date_str = st.text_input("Please enter a date (e.g. January 1, 2022): ")
+        if len(date_str) != 0:
+            p_date = parse_date(date_str)
+            if p_date is not None:
+                articles_of_date = self.search_articles_date(p_date)
+                if articles_of_date is not None and len(articles_of_date) != 0:
+                    df = pd.DataFrame(articles_of_date, columns=["Article Title", "Newspaper"])
+                    st.subheader(f"Articles published on {date_str}: ")
+                    st.dataframe(df, hide_index=True)
+                elif articles_of_date is not None and len(articles_of_date) == 0:
+                    st.error("No articles found.")
+            else:
+                st.write("Invalid date format. Please enter a date in the format 'Month day, year'.")
+
+    def handle_search_word_articles(self):
+        word = st.text_input("Please enter a word: ")
+        articles_of_word = self.search_articles_word(word)
+        if articles_of_word is not None and len(articles_of_word) != 0:
+            df = pd.DataFrame(articles_of_word, columns=["Article Title", "Newspaper", "Date"])
+            st.subheader(f"Articles containing the word '{word}': ")
+            st.dataframe(df, hide_index=True)
+        elif articles_of_word is not None and len(articles_of_word) == 0:
+            st.error("No articles found.")
