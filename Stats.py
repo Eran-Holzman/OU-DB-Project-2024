e152eeae794ed7f8446d7c221ca4af9685140e22
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-08-24 20:19:36 +0300

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -2,32 +2,51 @@
 #              This module is responsible for calculating db statistics                                    #
 #              The methods in this class implement the last requirements in the assignment                 #
 ############################################################################################################
-from DB_handler import DB_handler
+from db_handler import *
 
 
 class Stats:
     def __init__(self):
-        self.db_handler = DB_handler()
+        """
+        Initializes the Stats class with a DB_handler instance.
+        """
+        self.db_handler = DBHandler()
 
-    # Returns a table with 2 columns, the word and the length of word.
-    # This applies to all words in the database.
     def num_of_chars_per_word(self):
+        """
+        Returns a table with the word and the length of each word in the database.
+
+        Returns:
+            List[Tuple[str, int]]: A list of tuples, each containing a word and its length.
+        """
         self.db_handler.cursor.execute("""SELECT word, char_length(word) as word_length 
                                           FROM text_handle.words
                                           ORDER BY word_length""")
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
 
-    # Returns the average number of characters per word in the database.
     def avg_num_of_chars_per_word(self):
+        """
+        Calculates the average number of characters per word in the database.
+
+        Returns:
+            float: The average number of characters per word.
+        """
         self.db_handler.cursor.execute("SELECT ROUND(AVG(char_length(word)),2) "
                                        "FROM text_handle.words")
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()[0][0]
 
-    # Returns a table with 2 columns, the word and the length of word.
-    # This applies to all words in the article.
     def num_of_chars_per_word_in_article(self, article_title):
+        """
+        Returns a table with the word and the length of each word in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[str, int]]: A list of tuples, each containing a word and its length in the specified article.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -44,8 +63,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns the average number of characters per word in the article.
     def avg_num_of_chars_per_word_in_article(self, article_title):
+        """
+        Calculates the average number of characters per word in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            float: The average number of characters per word in the article.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -60,9 +87,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()[0][0]
 
-    # Returns the total number of characters for EACH line in the article.
-    # This INCLUDES spaces.
     def num_of_characters_in_line(self, article_title):
+        """
+        Calculates the total number of characters for each line in a specific article, including spaces.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[int, int, int]]: A list of tuples, each containing (paragraph_number, line_number, total_characters_in_line).
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -83,8 +117,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns the average number of characters for EACH line in the article.
     def avg_of_characters_in_line(self, article_title):
+        """
+        Calculates the average number of characters per line in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            float: The average number of characters per line.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -104,8 +146,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()[0][0]
 
-    # Returns the total number of characters in a paragraph
     def num_of_chars_in_paragraph(self, article_title):
+        """
+        Calculates the total number of characters in each paragraph of a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[int, int]]: A list of tuples, each containing (paragraph_number, total_characters_in_paragraph).
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -123,8 +173,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns the average number of characters in a paragraph
     def avg_chars_in_paragraph(self, article_title):
+        """
+        Calculates the average number of characters per paragraph in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            float: The average number of characters per paragraph.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -144,8 +202,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()[0][0]
 
-    # Returns the total number of characters in the article
     def num_of_chars_in_article(self, article_title):
+        """
+        Calculates the total number of characters in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            int: The total number of characters in the article.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -163,8 +229,13 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()[0][0]
 
-    # Returns the number of characters in the entire database(Of all the articles)
     def num_of_chars_in_db(self):
+        """
+        Calculates the total number of characters in the entire database (all articles).
+
+        Returns:
+            List[Tuple[int]]: A list containing a single tuple with the total character count.
+        """
         self.db_handler.cursor.execute("""
                                         SELECT SUM(total_chhars_per_article)
                                         FROM(
@@ -185,6 +256,12 @@ class Stats:
 
     # Returns the number of characters in the entire database(Of all the articles)
     def avg_chars_in_db(self):
+        """
+        Calculates the average number of characters per article in the entire database.
+
+        Returns:
+            float: The average number of characters per article.
+        """
         self.db_handler.cursor.execute("""
                                         SELECT ROUND(AVG(total_chhars_per_article),2)
                                         FROM(
@@ -205,15 +282,28 @@ class Stats:
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
 
-    # Returns the total number of words in the db)
     def num_of_words_in_db(self):
+        """
+        Counts the total number of unique words in the database.
+
+        Returns:
+            List[Tuple[int]]: A list containing a single tuple with the total word count.
+        """
         self.db_handler.cursor.execute("""SELECT COUNT(word_id)
                                           FROM text_handle.words""")
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
 
-    # Returns the total number of words in the article
     def num_of_words_in_article(self, article_title):
+        """
+        Counts the total number of words in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[int]]: A list containing a single tuple with the word count for the article.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -223,8 +313,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns the total number of words in the paragraph
     def num_of_words_in_paragraph(self, article_title):
+        """
+        Counts the number of words in each paragraph of a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[int]]: A list of tuples, each containing the word count for a paragraph.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -236,8 +334,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns the average number of words in the paragraph
     def avg_words_in_paragraph(self, article_title):
+        """
+        Calculates the average number of words per paragraph in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[float]]: A list containing a single tuple with the average word count per paragraph.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -249,8 +355,16 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns the total number of words in the line
     def num_of_words_in_line(self, article_title):
+        """
+        Counts the number of words in each line of a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[int, int, int]]: A list of tuples, each containing (paragraph_number, line_number, num_of_words_in_line).
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -264,6 +378,15 @@ class Stats:
 
     # Returns the average number of words in the line
     def avg_words_in_line(self, article_title):
+        """
+        Calculates the average number of words per line in a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[float]]: A list containing a single tuple with the average word count per line.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -277,8 +400,13 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
-    # Returns a frequency list of the words in the entire database(Meaning all the articles)
     def frequency_list_db(self):
+        """
+        Generates a frequency list of words for the entire database.
+
+        Returns:
+            List[Tuple[int, str, int]]: A list of tuples, each containing (row_number, word, frequency) for all words in the database.
+        """
         self.db_handler.cursor.execute("""  SELECT ROW_NUMBER() OVER 
                                             (ORDER BY word) AS row_number, word, COUNT(word) AS frequency 
                                             FROM text_handle.words_positions
@@ -286,8 +414,16 @@ class Stats:
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
 
-    # Returns a frequency list of the words in the article
     def frequency_list_article(self, article_title):
+        """
+        Generates a frequency list of words for a specific article.
+
+        Args:
+            article_title (str): The title of the article.
+
+        Returns:
+            List[Tuple[int, str, int]]: A list of tuples, each containing (row_number, word, frequency) for all words in the specified article.
+        """
         article_id_full = self.db_handler.get_article_id_from_title(article_title)
         if article_id_full:
             article_id = article_id_full[0][0]
@@ -300,5 +436,14 @@ class Stats:
             self.db_handler.connection.commit()
             return self.db_handler.cursor.fetchall()
 
+    def get_total_articles(self):
+        """
+        Get the total number of articles in the database.
+
+        Returns:
+            int: The total number of articles.
+        """
+        self.db_handler.cursor.execute("SELECT COUNT(*) FROM art_info.articles")
+        return self.cursor.fetchone()[0]
 
 
